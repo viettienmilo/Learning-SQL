30ec0c4bae4afa002dd6113291e4ad6efa256099
viettienmilo <viettien.milo@gmail.com>
2026-02-16 13:46:22 +0700

    update

---
+++ sql_for_da/c3_plot.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+df1 = pd.read_csv("sql_for_da/c3_plot_1.csv")
+fig1 = px.line(df1, x='YEAR(sales_month)', y='SUM(sales)', color='kind_of_business',
+              title='TOTAL SALES BY YEAR')
+fig1.show()
+
+df2 = pd.read_csv("sql_for_da/c3_plot_2.csv")
+fig2 = px.line(df2, x='YEAR(sales_month)', y='SUM(sales)', color='kind_of_business',
+              title='TOTAL SALES BY YEAR')
+fig2.show()
+
+df3 = pd.read_csv("sql_for_da/c3_plot_3.csv")
+fig3 = make_subplots(rows=3, cols=1, subplot_titles=(
+    "SALES GAP BETWEEN WOMEN SALES AND MEN SALES",
+    "TIMES OF WOMEN SALES PER MEN SALES",
+    "PERCENTAGE OF WOMEN SALES PER MEN SALES"
+))
+fig3.add_trace(go.Scatter(x=df3['Year'], 
+                          y=df3['Women/Men Diff'],
+                          mode='lines',
+                          name='Women/Men Diff'),
+                          row=1,
+                          col=1)
+fig3.add_trace(go.Scatter(x=df3['Year'], 
+                          y=df3['Women/Men Times'],
+                          mode='lines',
+                          name='Women/Men Times'),
+                          row=2,
+                          col=1)
+fig3.add_trace(go.Scatter(x=df3['Year'], 
+                          y=df3['Women/Men %'],
+                          mode='lines',
+                          name='Women/Men %'),
+                          row=3,
+                          col=1)
+fig3.update_layout(title="COMPARISION BETWEEN YEARLY WOMEN/MEN SALES")
+fig3.show()
--- sql_for_da/c3_timeseries.sql
+++ sql_for_da/c3_timeseries.sql
@@ -1,7 +1,10 @@
 CREATE DATABASE IF NOT EXISTS sql_da;
+
 USE sql_da;
+
 --- create table
 DROP TABLE IF EXISTS retail_sales;
+
 CREATE TABLE retail_sales (
     sales_month date,
     naics_code varchar(256),
@@ -9,27 +12,124 @@ CREATE TABLE retail_sales (
     reason_for_null varchar(256),
     sales decimal(10, 2)
 );
+
+-- import data from csv file
+SELECT
+    @ @secure_file_priv;
+
+--C:\ProgramData\MySQL\MySQL Server 9.4\Uploads\
+LOAD DATA INFILE "C:\\ProgramData\\MySQL\\MySQL Server 9.4\\Uploads\\us_retail_sales.csv" INTO TABLE retail_sales FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
 -- preview data
-SELECT *
-FROM retail_sales
-LIMIT 10;
+SELECT
+    *
+FROM
+    retail_sales
+LIMIT
+    10;
+
 -- total rows
-SELECT COUNT(*)
-FROM retail_sales;
+SELECT
+    COUNT(*)
+FROM
+    retail_sales;
+
 -- earliest/latest date
-SELECT MIN(sales_month) AS EARLIEST_DATE,
+SELECT
+    MIN(sales_month) AS EARLIEST_DATE,
     MAX(sales_month) AS LATEST_DATE
-FROM retail_sales;
+FROM
+    retail_sales;
+
 /*#############################################
  TRENDING ANALYSIS
  ##############################################*/
 -- The trend of total retail and food services sales in the US
-SELECT sales_month,
+SELECT
+    sales_month,
     sales
-FROM retail_sales
-WHERE kind_of_business = 'Retail and food services sales, total';
+FROM
+    retail_sales
+WHERE
+    kind_of_business = 'Retail and food services sales, total';
+
 -- Group by year
-SELECT YEAR(sales_month),
+SELECT
+    YEAR(sales_month),
     SUM(sales)
-FROM retail_sales
-GROUP BY 1;
+FROM
+    retail_sales
+GROUP BY
+    1;
+
+/* 
+ Compare sales between year and kind_of_bussiness of 'Book store',
+ 'Sporting goods stores', 'Hobby, toy, and game stores'
+ (Result in file c3_plot_1.csv) 
+ */
+SELECT
+    YEAR(sales_month),
+    kind_of_business,
+    SUM(sales)
+FROM
+    retail_sales
+WHERE
+    kind_of_business IN (
+        'Book stores',
+        'Sporting goods stores',
+        'Hobby, toy, and game stores'
+    )
+GROUP BY
+    1,
+    2;
+
+/*
+ Compare sales between men and women clothing stores
+ (Result in file c3_plot_2.csv)
+ */
+SELECT
+    YEAR(sales_month),
+    kind_of_business,
+    SUM(sales)
+FROM
+    retail_sales
+WHERE
+    kind_of_business IN (
+        "Men's clothing stores",
+        "Women's clothing stores"
+    )
+GROUP BY
+    1,
+    2;
+
+-- pivot and compare sales between Men and Women group
+-- (Result IN FILE c3_plot_3.csv)
+WITH pivot_table AS (
+    SELECT
+        YEAR(sales_month) AS `Year`,
+        SUM(
+            CASE
+                WHEN kind_of_business = "Men's clothing stores" THEN sales
+            END
+        ) AS `Men's sales`,
+        SUM(
+            CASE
+                WHEN kind_of_business = "Women's clothing stores" THEN sales
+            END
+        ) AS `Women's sales`
+    FROM
+        retail_sales
+    WHERE
+        sales_month <= '2019-12-01'
+    GROUP BY
+        1
+)
+SELECT
+    Year,
+    `Men's sales`,
+    `Women's sales`,
+    (`Women's sales` - `Men's sales`) AS `Women/Men Diff`,
+    (`Women's sales` / `Men's sales`) AS `Women/Men Times`,
+    (`Women's sales` / `Men's sales` - 1) * 100 AS `Women/Men %`
+FROM
+    pivot_table;
